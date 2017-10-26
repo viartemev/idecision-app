421e3ce0d7d0e6a96d7d29533d179e877247f244
Vyacheslav Artemyev <vyacheslav.artemyev@zalando.fi>
2017-10-26 21:38:41 +0300

    add modal style

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -6,10 +6,12 @@ const OptionModal = (props) => (
         isOpen={!!props.selectedOption}
         contentLabel="Selection Option"
         onRequestClose={props.handleClearSelectedOption}
+        closeTimeoutMS={200}
+        className="modal"
     >
-        <h3>Selection Options</h3>
-        {props.selectedOption && <p>{props.selectedOption}</p>}
-        <button onClick={props.handleClearSelectedOption}>Okey</button>
+        <h3 className="modal__title">Selection Options</h3>
+        {props.selectedOption && <p className="modal__body">{props.selectedOption}</p>}
+        <button className="button" onClick={props.handleClearSelectedOption}>Okey</button>
     </Modal>
 );
 
